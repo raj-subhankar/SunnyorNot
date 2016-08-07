b4efd4ff5c956bd14877cff1e6150cecc1cb05bd
Subhankar Raj <subhankar.rj@gmail.com>
2016-08-07 22:29:29 +0530

    Modify WeatherDbHelper.java

--- app/src/test/java/com/android/subhankar/sunnyornot/ExampleUnitTest.java
+++ app/src/test/java/com/android/subhankar/sunnyornot/ExampleUnitTest.java
@@ -1,15 +1,22 @@
 package com.android.subhankar.sunnyornot;
 
-import org.junit.Test;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
     }
 }
